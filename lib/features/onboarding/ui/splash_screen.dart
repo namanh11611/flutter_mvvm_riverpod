4bafed7bd69280f69e00ebda1dff4442f92e2aeb
Henry <namanh11611@gmail.com>
2025-07-25 23:15:35 +0700

    Update SplashScreen

--- lib/features/onboarding/ui/splash_screen.dart
+++ lib/features/onboarding/ui/splash_screen.dart
@@ -3,16 +3,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../constants/constants.dart';
-import '../../../features/authentication/repository/authentication_repository.dart';
-import '../../../features/common/ui/widgets/loading.dart';
 import '../../../routing/routes.dart';
+import '../../authentication/repository/authentication_repository.dart';
+import '../../common/ui/widgets/loading.dart';
 
-class SplashScreen extends ConsumerWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    _checkLoginStatus(ref, context);
+  ConsumerState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkLoginStatus();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Loading(),
@@ -20,11 +32,11 @@ class SplashScreen extends ConsumerWidget {
     );
   }
 
-  Future<void> _checkLoginStatus(WidgetRef ref, BuildContext context) async {
+  Future<void> _checkLoginStatus() async {
     final isLoggedIn = await ref.read(authenticationRepositoryProvider).isLogin();
     debugPrint('${Constants.tag} [SplashScreen._checkLoginStatus] isLoggedIn = $isLoggedIn');
     await Future.delayed(const Duration(seconds: 1));
-    if (!context.mounted) return;
+    if (!mounted) return;
     if (isLoggedIn) {
       context.pushReplacement(Routes.main);
     } else {
